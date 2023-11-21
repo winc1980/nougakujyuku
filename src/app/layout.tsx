4eed467335829891c361d93978975242bf6a6434
Yuma Morimoto <kanten520212@gmail.com>
2023-11-21 17:47:55 +0900

    Bootstrapの追加 & tailwindcssの削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -7,9 +8,16 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Home',
   description: 'microCMS and Next.js',
+  openGraph: {
+    title: '農楽塾',
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>

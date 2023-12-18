6783c2e0e775f38942c296ef7e0edd76534c0c82
rotation <prometheus4e7e0811@icloud.com>
2023-12-18 23:26:03 +0900

    レスポンシブ対応完了

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 {
-  let targets = document.querySelectorAll('.img_2,.img_3,.img_4'); //アニメーションさせたい要素
+  let targets = document.querySelectorAll('.img_2,.img_3,.img_4,.small_eclipse1,.small_eclipse2,.big_eclipse,.medium_eclipse'); //アニメーションさせたい要素
       //スクロールイベント
       window.addEventListener('scroll', function () {
         var scroll = window.scrollY; //スクロール量を取得
@@ -11,20 +11,24 @@
           }
         }
       });
-  let targets_ = document.querySelectorAll('.small_eclipse1,.small_eclipse2,.big_eclipse,.medium_eclipse'); //アニメーションさせたい要素
-      //スクロールイベント
-      window.addEventListener('scroll', function () {
-        var scroll = window.scrollY; //スクロール量を取得
-        var windowHeight = window.innerHeight; //画面の高さを取得
-        for (let target_ of targets_) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
-          var targetPos_ = target_.getBoundingClientRect().top + scroll; //ターゲット要素の位置を取得
-          if (scroll > targetPos_ - windowHeight) { //スクロール量 > ターゲット要素の位置
-            target_.classList.add('is-focused'); //is-animatedクラスを加える
-          }
-        }
-      });
-      let _targets_ = document.querySelectorAll('.keyword1,.keyword2');
+  // let targets_ = document.querySelectorAll(''); //アニメーションさせたい要素
+  //     //スクロールイベント
+  //     window.addEventListener('scroll', function () {
+  //       var scroll = window.scrollY; //スクロール量を取得
+  //       var windowHeight = window.innerHeight; //画面の高さを取得
+  //       for (let target_ of targets_) { //ターゲット要素がある分、アニメーション用のクラスをつける処理を繰り返す
+  //         var targetPos_ = target_.getBoundingClientRect().top + scroll; //ターゲット要素の位置を取得
+  //         if (scroll > targetPos_ - windowHeight) { //スクロール量 > ターゲット要素の位置
+  //           target_.classList.add('is-focused'); //is-animatedクラスを加える
+  //         }
+  //       }
+  //     });
+      let _targets_ = document.querySelectorAll('.keyword1');
         for (let _target_ of _targets_) {
           _target_.classList.add('fade-in_left'); //fade-in_leftクラスを加える
+      }
+      let _targets__ = document.querySelectorAll('.keyword2');
+        for (let _target__ of _targets__) {
+          _target__.classList.add('fade-in_left2'); //fade-in_leftクラスを加える
       }
   }
